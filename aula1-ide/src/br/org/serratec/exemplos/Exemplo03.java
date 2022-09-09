91b24df43b2be08392d4867f9634d7d3ab928f61
CleiverSoares <110869785+CleiverSoares@users.noreply.github.com>
2022-09-08 22:51:05 -0300

    Add files via upload

---
+++ aula1-ide/src/br/org/serratec/exemplos/Exemplo03.java
@@ -0,0 +1,15 @@
+package br.org.serratec.exemplos;
+
+public class Exemplo03 {
+
+	public static void main(String[] args) {
+		System.out.println("Programação em Java - Aula-9");
+		System.out.println("Programação em Java - Aula-9");
+		System.out.println("Programação em Java - Aula-9");
+		System.out.println("Programação em Java - Aula-9");
+		System.out.println("Programação em Java - Aula-9");
+		System.out.println("Programação em Java - Aula-9");
+
+	}
+
+}
